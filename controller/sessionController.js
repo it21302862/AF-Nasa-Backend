411e9c0905235ae163cce831a3969b4ee5e275b9
Chamaththa123 <shamodchamaththa@gmail.com>
2024-03-24 15:26:58 +0530

    session complete

--- controller/sessionController.js
+++ controller/sessionController.js
@@ -1,57 +1,100 @@
-const sessionService = require('../service/sessionService');
+const sessionService = require("../service/sessionService");
 
+// Controller function to create a new session
 const createSession = async (req, res, next) => {
-    try {
-        const sessionData = req.body;
-        const session = await sessionService.createSession(sessionData);
-        res.status(201).json(session);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const sessionData = req.body;
+    const session = await sessionService.createSession(sessionData);
+    res.status(201).json(session);
+  } catch (error) {
+    next(error);
+  }
 };
 
+// Controller function to get all sessions
 const getAllSessions = async (req, res, next) => {
-    try {
-        const sessions = await sessionService.getAllSessions();
-        res.status(200).json(sessions);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const sessions = await sessionService.getAllSessions();
+    res.status(200).json(sessions);
+  } catch (error) {
+    next(error);
+  }
 };
 
+// Controller function to get sessions by class room ID
+const getSessionByClassRoomId = async (req, res, next) => {
+  try {
+    const classRoomId = req.params.classRoomId;
+    const sessions = await sessionService.getSessionByClassRoomId(classRoomId);
+
+    if (!sessions) {
+      return res.status(404).json({ error: "Session not found" });
+    }
+
+    res.status(200).json(sessions);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Function to get all available sessions
+const getAvailableSessions = async (req, res, next) => {
+  try {
+    const availableSessions = await sessionService.getAvailableSessions();
+    res.status(200).json(availableSessions);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Function to get all booked sessions
+const getBookedSessions = async (req, res, next) => {
+  try {
+    const bookedSessions = await sessionService.getBookedSessions();
+    res.status(200).json(bookedSessions);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Controller function to update a session
 const updateSession = async (req, res, next) => {
-    try {
-        const sessionId = req.params.id;
-        const sessionData = req.body;
-        const updatedSession = await sessionService.updateSession(sessionId, sessionData);
+  try {
+    const sessionId = req.params.id;
+    const sessionData = req.body;
+    const updatedSession = await sessionService.updateSession(
+      sessionId,
+      sessionData
+    );
 
-        if (!updatedSession) {
-            throw new Error('Session not found');
-        }
-
-        res.status(200).json(updatedSession);
-    } catch (error) {
-        next(error);
+    if (!updatedSession) {
+      return res.status(404).json({ error: "Session not found" });
     }
+
+    res.status(200).json(updatedSession);
+  } catch (error) {
+    next(error);
+  }
 };
 
+// Controller function to delete a session
 const deleteSession = async (req, res, next) => {
-    try {
-        const sessionId = req.params.id;
-        const deletedSession = await sessionService.deleteSession(sessionId);
-        
-        if (!deletedSession) {
-            throw new Error('Session not found');
-        }
-        res.status(200).json("removed session successfully");
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const sessionId = req.params.id;
+    const deletedSession = await sessionService.deleteSession(sessionId);
+
+    res.status(200).json({ message: "Session removed successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    createSession,
-    getAllSessions,
-    updateSession,
-    deleteSession
+  createSession,
+  getAllSessions,
+  getSessionByClassRoomId,
+  getAvailableSessions,
+  getBookedSessions,
+  updateSession,
+  deleteSession,
 };
--- service/sessionService.js
+++ service/sessionService.js
@@ -1,60 +1,115 @@
-const Session = require('../model/sessionModel');
+const Session = require("../model/sessionModel");
 
+// Function to create a new session
 const createSession = async (sessionData) => {
-    try {
-        // Check if there is any existing session that conflicts with the new session
-        const existingSession = await Session.findOne({
-            dayOfWeek: sessionData.dayOfWeek,
-            $or: [
-                { $and: [{ startTime: { $lte: sessionData.startTime } }, { endTime: { $gt: sessionData.startTime } }] },
-                { $and: [{ startTime: { $lt: sessionData.endTime } }, { endTime: { $gte: sessionData.endTime } }] },
-                { $and: [{ startTime: { $gte: sessionData.startTime } }, { endTime: { $lte: sessionData.endTime } }] }
-            ],
-            classRoomId: sessionData.classRoomId
-        });
+  try {
+    // Check if there is any existing session that conflicts with the new session
+    const existingSession = await Session.findOne({
+      dayOfWeek: sessionData.dayOfWeek,
+      $or: [
+        {
+          $and: [
+            { startTime: { $lte: sessionData.startTime } },
+            { endTime: { $gt: sessionData.startTime } },
+          ],
+        },
+        {
+          $and: [
+            { startTime: { $lt: sessionData.endTime } },
+            { endTime: { $gte: sessionData.endTime } },
+          ],
+        },
+        {
+          $and: [
+            { startTime: { $gte: sessionData.startTime } },
+            { endTime: { $lte: sessionData.endTime } },
+          ],
+        },
+      ],
+      classRoomId: sessionData.classRoomId,
+    });
 
-        if (existingSession) {
-            const conflictMessage = `Conflict with existing session: Day: ${existingSession.dayOfWeek}, Start Time: ${existingSession.startTime}, End Time: ${existingSession.endTime}, Room ID: ${existingSession.classRoomId}`;
-            throw new Error(conflictMessage);
-        }
-
-        // Create the new session
-        const session = await Session.create(sessionData);
-        return session;
-    } catch (error) {
-        throw error;
+    if (existingSession) {
+      const conflictMessage = `Conflict with existing session: Day: ${existingSession.dayOfWeek}, Start Time: ${existingSession.startTime}, End Time: ${existingSession.endTime}, Room ID: ${existingSession.classRoomId}`;
+      throw new Error(conflictMessage);
     }
+
+    // Create the new session
+    const session = await Session.create(sessionData);
+    return session;
+  } catch (error) {
+    throw error;
+  }
 };
 
+// Function to get all sessions
 const getAllSessions = async () => {
-    try {
-        const sessions = await Session.find();
-        return sessions;
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const sessions = await Session.find();
+    return sessions;
+  } catch (error) {
+    throw error;
+  }
 };
 
+// Function to get sessions by class room ID
+const getSessionByClassRoomId = async (classRoomId) => {
+  try {
+    const sessions = await Session.find({ classRoomId });
+    return sessions;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Function to get all available sessions
+const getAvailableSessions = async () => {
+  try {
+    const availableSessions = await Session.find({ isBooked: false });
+    return availableSessions;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Function to get all booked sessions
+const getBookedSessions = async () => {
+  try {
+    const bookedSessions = await Session.find({ isBooked: true });
+    return bookedSessions;
+  } catch (error) {
+    throw error;
+  }
+};
+// Function to update a session
 const updateSession = async (sessionId, sessionData) => {
-    try {
-        const updatedSession = await Session.findByIdAndUpdate(sessionId, sessionData, { new: true });
-        return updatedSession;
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const updatedSession = await Session.findByIdAndUpdate(
+      sessionId,
+      sessionData,
+      { new: true }
+    );
+    return updatedSession;
+  } catch (error) {
+    throw error;
+  }
 };
 
+// Function to delete a session
 const deleteSession = async (sessionId) => {
-    try {
-        await Session.findByIdAndDelete(sessionId);
-    } catch (error) {
-        throw error;
-    }
+  try {
+    await Session.findByIdAndDelete(sessionId);
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = {
-    createSession,
-    getAllSessions,
-    updateSession,
-    deleteSession
+  createSession,
+  getAllSessions,
+  getSessionByClassRoomId,
+  updateSession,
+  deleteSession,
+  getAvailableSessions,
+  getBookedSessions,
 };
